3b610ee0985993147759d9214785fbe370cc29b2
gganster <guilian.ganster@gmail.com>
2024-02-24 05:51:50 +0100

    parralel+interceptor modal

---
+++ app/@modal/(.)write/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createPost } from '@/app/write/write-post.action';
+import { getUser } from "@/src/feature/query/user.query";
+import { WriteModal } from './WriteModal';
+
+export default async function page() {
+  const user = await getUser();
+
+  return (
+    <WriteModal user={user} createPost={async (v) => {
+      "use server";
+      return createPost(v);
+    }} />
+  )
+}
