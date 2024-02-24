8d55ee8d722267b7a9fd2f52711508757894560a
gganster <guilian.ganster@gmail.com>
2024-02-24 03:24:16 +0100

    Layout & add post actions

--- src/feature/post/Post.tsx
+++ src/feature/post/Post.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {PostHome} from "@/src/feature/query/post.query";
 import { PostLayout } from '@/src/feature/post/PostLayout';
 import Link from 'next/link';
+import { Heart, MessageCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type PostProps = {
   post: PostHome;
@@ -13,6 +15,22 @@ export const Post = ({post} : PostProps) => {
       <Link href={`/posts/${post.id}`} className="text-sm text-foreground">
         {post.content}
       </Link>
+      <div className="flex items-center">
+        <Button size="icon" variant="ghost">
+          <Heart size={20} />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <MessageCircle size={20} />
+        </Button>
+      </div>
+      <div className="flex items-center gap-1">
+        <Link className="text-muted-foreground text-sm" href={`/posts/${post.id}`} className="text-sm text-muted-foreground">
+          {post._count.likes} likes
+        </Link>
+        <Link className="text-muted-foreground text-sm" href={`/posts/${post.id}`} className="text-sm text-muted-foreground">
+          {post._count.replies} replies
+        </Link>
+      </div>
     </PostLayout>
   );
 };
